255b3b885b4fbf842cbe092fbd96174f7c329ded
Khoa Pham <onmyway133@gmail.com>
2018-04-11 16:55:53 +0200

    Add CheersAlone page

--- MyPlayground.playground/Pages/Cheers.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Cheers.xcplaygroundpage/Contents.swift
@@ -1,20 +1,10 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-import Cheers
 import AppFramework
 import PlaygroundSupport
 
-let cheerView = CheerView()
-cheerView.frame = CGRect(x: 0, y: 50, width: 200, height: 400)
-
-// Configure
-cheerView.config.particle = .confetti(allowedShapes: [.rectangle, .circle])
-
-// Start
-cheerView.start()
-
-let myController = ViewController()
-
-PlaygroundPage.current.liveView = myController.view
+let controller = ViewController()
+controller.view.frame.size = CGSize(width: 375, height: 667)
+PlaygroundPage.current.liveView = controller.view
 
---
+++ MyPlayground.playground/Pages/CheersAlone.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+import Cheers
+import AppFramework
+import PlaygroundSupport
+
+// Use cheer alone
+let cheerView = CheerView()
+cheerView.frame = CGRect(x: 0, y: 50, width: 200, height: 400)
+
+// Configure
+cheerView.config.particle = .confetti(allowedShapes: [.rectangle, .circle])
+
+// Start
+cheerView.start()
+
+PlaygroundPage.current.liveView = cheerView
+
