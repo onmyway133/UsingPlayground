fda1f4b2df7bbd94b1e0c2386829a719b8e62591
Khoa Pham <onmyway133@gmail.com>
2018-04-12 08:08:35 +0200

    Update

--- MyPlayground.playground/Pages/Trait.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Trait.xcplaygroundpage/Contents.swift
@@ -9,12 +9,18 @@ let controller = ResourceViewController()
 let parent = UIViewController()
 parent.addChildViewController(controller)
 parent.view.addSubview(controller.view)
+controller.didMove(toParentViewController: parent)
 
 let traits = UITraitCollection.init(traitsFrom: [
   .init(horizontalSizeClass: .compact),
   .init(verticalSizeClass: .regular),
   .init(userInterfaceIdiom: .phone)
 ])
+
+controller.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+parent.view.frame = CGRect(x: 0, y: 0, width: 375, height: 667)
+controller.view.frame = parent.view.frame
+parent.preferredContentSize = parent.view.frame.size
 
 parent.setOverrideTraitCollection(traits, forChildViewController: controller)
 
